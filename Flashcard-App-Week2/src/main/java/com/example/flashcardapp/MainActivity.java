3ff41b47c1f358373cd1222a5b5fb96aebf82097
skaur10 <sk.kaur78@gmail.com>
2020-02-22 01:58:31 -0500

    Add files via upload

---
+++ Flashcard-App-Week2/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.flashcardapp;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+}
