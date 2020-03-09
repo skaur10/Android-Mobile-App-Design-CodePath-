da2dc6429b7b09af56d4a825be5a55fcdda3b406
Simranjit Kaur <sk.kaur78@gmail.com>
2020-03-09 00:18:50 -0400

    Add files via upload

---
+++ Flashcard-App-Week3/main/java/com/example/flashcard/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.flashcard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.eye_visible_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("stringKey1", "harry potter");
+                intent.putExtra("stringKey2", "voldemort");
+                MainActivity.this.startActivityForResult(intent,100);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK) {
+            String newQuestion = data.getExtras().getString("question");
+            String newAnswer = data.getExtras().getString("answer");
+            ((TextView) findViewById(R.id.flashcard_question)).setText(newQuestion);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(newAnswer);
+        }
+    }
+}
